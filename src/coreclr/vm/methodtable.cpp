ac29b5567fbad18bfa6e6083d9a4842189101b42
David Wrighton <davidwr@microsoft.com>
2021-04-22 13:50:42 -0700

    Handle generic methods (#4)

--- src/coreclr/vm/methodtable.cpp
+++ src/coreclr/vm/methodtable.cpp
@@ -9246,7 +9246,7 @@ MethodTable::TryResolveVirtualStaticMethodOnThisType(MethodDesc* pInterfaceMD)
         {
             COMPlusThrow(kTypeLoadException, E_FAIL);
         }
-        if (pMethodDecl != pInterfaceMD)
+        if (!pMethodDecl->HasSameMethodDefAs(pInterfaceMD))
         {
             continue;
         }
@@ -9260,7 +9260,15 @@ MethodTable::TryResolveVirtualStaticMethodOnThisType(MethodDesc* pInterfaceMD)
         {
             COMPlusThrow(kTypeLoadException, E_FAIL);
         }
-        return pMethodImpl;
+
+        if (pInterfaceMD->HasMethodInstantiation())
+        {
+            return pMethodImpl->FindOrCreateAssociatedMethodDesc(pMethodImpl, this, FALSE, pInterfaceMD->GetMethodInstantiation(), TRUE);
+        }
+        else
+        {
+            return pMethodImpl;
+        }
     }
 
     return nullptr;
